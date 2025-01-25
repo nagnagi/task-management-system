ccf8bd6d93b3f535d10d85c387d564776a0b75bb
nagnagi <nag.nagipon@gmail.com>
2025-01-25 16:11:02 +0900

    DBのすべての要素を表示するメソッドを追加

--- server/database/database.py
+++ server/database/database.py
@@ -3,9 +3,18 @@ from pathlib import Path
 
 class DataBase:
     def __init__(self):
-        dbpath = str(Path(__file__).parent) + "data.sqlite"
+        dbpath = str(Path(__file__).parent) + '/data.sqlite'
         self.connection = sqlite3.connect(dbpath)
         self.cursor = self.connection.cursor()
+
+    def show_table(self, table_name: str) -> list[tuple]:
+        return self.send_query('select * from ' + table_name)
+
+    def send_query(self, query: str) -> list[tuple]:
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        self.connection.commit()
+        return result
 
     def __del__(self):
         self.cursor.close()
