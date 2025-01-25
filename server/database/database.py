15560e3eb7d1c0d096a208e96b649be56ce21d15
nagnagi <nag.nagipon@gmail.com>
2025-01-25 14:29:36 +0900

    DBを操作するクラスを作成

--- server/database/database.py
+++ server/database/database.py
@@ -1,1 +1,12 @@
 import sqlite3
+from pathlib import Path
+
+class DataBase:
+    def __init__(self):
+        dbpath = str(Path(__file__).parent) + "data.sqlite"
+        self.connection = sqlite3.connect(dbpath)
+        self.cursor = self.connection.cursor()
+
+    def __del__(self):
+        self.cursor.close()
+        self.connection.close()
