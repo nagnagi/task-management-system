4d6e4d525b473a89de42b95240ba6e2d49013b26
nagnagi <nag.nagipon@gmail.com>
2025-01-25 20:44:14 +0900

    ディクショナリに変換するメソッドを作成

--- server/database/dataclasses/data.py
+++ server/database/dataclasses/data.py
@@ -1,7 +1,10 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 
 @dataclass
 class Data:
+    def to_dict(self) -> dict:
+        return asdict(self)
+    
     @classmethod
     def from_tuple(cls, obj: tuple) -> 'Data':
         return cls(*obj)
--- server/test/testdataclass.py
+++ server/test/testdataclass.py
@@ -15,7 +15,8 @@ task_db.insert_task(2, '2025-01-25', "Test Task 0", "TEST DAYO!", 1)
 task_db.insert_task(1, '2025-01-25', "Test Task 1", "Motto TEST DAYO!", 1)
 
 print(task_db[1])
-print(task_db.at_to_data(2))
+print(task_db.at_to_data(2).name)
 print(task_db.at(1))
+print(task_db[2].to_dict())
 
 task_db.delete_all()
