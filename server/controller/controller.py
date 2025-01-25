9db1e5b383be54404f00398b3805fe7a4c615aac
nagnagi <nag.nagipon@gmail.com>
2025-01-26 08:29:11 +0900

    起動スクリプトを作成

--- server/controller/controller.py
+++ server/controller/controller.py
@@ -1,2 +1,13 @@
 import sys
 import os
+
+from controller.commandline import CommandLine
+
+def execute_with_commandline():
+    commandline = CommandLine()
+    try:
+        while True:
+            if result := commandline.input():
+                print(result)
+    except KeyboardInterrupt:
+        return
