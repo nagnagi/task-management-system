02e354b31052f2a558a7aa7b00b9e4b3f06a63a2
nagnagi <nag.nagipon@gmail.com>
2025-01-26 17:32:26 +0900

    idからtodo文字列を生成するメソッドを作成

--- server/pdf/latex.py
+++ server/pdf/latex.py
@@ -1,13 +1,37 @@
 import subprocess
 from pathlib import Path
 
+from controller.commandline import CommandLine
+
+from database.dataclasses.todo import ToDo
+from database.dataclasses.task import Task
+from database.dataclasses.project import Project
+
 class Latex:
     def __init__(self):
         self.parent = str(Path(__file__).parent)
         self.outdir = self.parent + '/out/'
         self.file = self.parent + '/main.tex'
 
-    def compile(self):
+    def compile(self) -> str:
         cmd = 'lualatex --output-directory=' + self.outdir + ' ' + self.file
         print(cmd)
         subprocess.call(cmd)
+        return cmd
+
+    def from_id(self, id: int) -> list[str]:
+        commandline = CommandLine()
+        todo: list[ToDo] = commandline.exec('list todo ' + str(id))
+
+        lines: list[str] = []
+        for item in todo:
+            task: Task = commandline.exec('get task ' + str(item.task_id))
+            fin = '\u2611' if task.fin else '\u25a1'
+            priority = chr(ord('A') + task.priority)
+            add_date = task.add_date
+            fin_date = task.fin_date if task.fin_date else ''
+            name = task.name
+            project_name = commandline.exec('get project ' + str(task.project_id)).name
+            lines.append(f'{fin} ({priority}) {add_date} {fin_date} {name} {project_name}')
+
+        return lines
--- server/test/testlatex.py
+++ server/test/testlatex.py
@@ -5,7 +5,11 @@ from icecream import ic
 sys.path.append(str(Path(__file__).parent.parent))
 
 from pdf.latex import Latex
+from controller.commandline import CommandLine
+
+commandline = CommandLine()
 
 engine = Latex()
 
+ic(engine.from_id(commandline.exec('get todolist ' + str(2)).todo_id))
 engine.compile()
